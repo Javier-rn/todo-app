0171a2780fea1eb94f9e5ef4bbf7903523aabb50
Javier-rn <javier96rn@gmail.com>
2023-05-02 16:16:44 +0200

    Prevent more folder inputs being added if one already exists

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -3,12 +3,28 @@ function addFolderBtnEvent() {
 
   addFolderBtn.addEventListener('click', function (e) {
     const folders = document.querySelector('#folders');
-    const input = document.createElement('input');
-    input.setAttribute('type', 'text');
-    const li = document.createElement('li');
-    li.append(input);
-    folders.append(li);
+
+    if (folders.children[folders.children.length - 1].children.length === 2) {
+      console.log('nono');
+    } else {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'text');
+      const li = document.createElement('li');
+      li.append(input);
+
+      const addInputBtn = document.createElement('button');
+      addInputBtn.textContent = '+';
+
+      li.append(addInputBtn);
+      folders.append(li);
+    }
   });
+}
+
+function removeEmptyInput(input) {
+  if (input.textContent === '') {
+    input.remove();
+  }
 }
 
 export default addFolderBtnEvent;
